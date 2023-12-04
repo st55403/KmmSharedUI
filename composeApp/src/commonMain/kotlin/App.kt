2e70651ab3cd533e6c203485598602e698a7da3a
Oleg Golovkov <oleg.golovkov@applifting.cz>
2023-12-04 19:58:09 +0100

    changed: Improved the style

--- composeApp/src/commonMain/kotlin/App.kt
+++ composeApp/src/commonMain/kotlin/App.kt
@@ -1,4 +1,6 @@
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
@@ -8,6 +10,12 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import kotlinx.datetime.Clock
 import kotlinx.datetime.IllegalTimeZoneException
 import kotlinx.datetime.LocalTime
@@ -20,18 +28,30 @@ fun App() {
         var location by remember { mutableStateOf("Europe/Paris") }
         var timeAtLocation by remember { mutableStateOf("No location selected") }
 
-        Column {
-            Text(timeAtLocation)
+        Column(modifier = Modifier.padding(20.dp)) {
+            Text(
+                timeAtLocation,
+                style = TextStyle(fontSize = 20.sp),
+                textAlign = TextAlign.Center,
+                modifier = Modifier.fillMaxWidth()
+                    .align(Alignment.CenterHorizontally)
+            )
+
             TextField(
                 value = location,
+                modifier = Modifier.padding(top = 10.dp),
                 onValueChange = {
                     location = it
                 }
             )
-            Button(onClick = {
-                timeAtLocation = currentTimeAt(location) ?: "Invalid location"
-            }) {
-                Text("Show Time At Location")
+
+            Button(
+                modifier = Modifier.padding(top = 10.dp),
+                onClick = {
+                    timeAtLocation = currentTimeAt(location) ?: "Invalid Location"
+                }
+            ) {
+                Text("Show Time")
             }
         }
     }
