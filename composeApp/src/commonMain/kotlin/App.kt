03bb8a7d5c34d053bd8d8e7b83ad547d6a782ac1
Oleg Golovkov <oleg.golovkov@applifting.cz>
2023-12-04 20:01:01 +0100

    changed: Refactored the design

--- composeApp/src/commonMain/kotlin/App.kt
+++ composeApp/src/commonMain/kotlin/App.kt
@@ -1,10 +1,12 @@
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Button
+import androidx.compose.material.DropdownMenu
+import androidx.compose.material.DropdownMenuItem
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
-import androidx.compose.material.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -17,15 +19,33 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import kotlinx.datetime.Clock
-import kotlinx.datetime.IllegalTimeZoneException
 import kotlinx.datetime.LocalTime
 import kotlinx.datetime.TimeZone
 import kotlinx.datetime.toLocalDateTime
 
+data class Country(val name: String, val zone: TimeZone)
+
+fun currentTimeAt(location: String, zone: TimeZone): String {
+    fun LocalTime.formatted() = "$hour:$minute:$second"
+
+    val time = Clock.System.now()
+    val localTime = time.toLocalDateTime(zone).time
+
+    return "The time in $location is ${localTime.formatted()}"
+}
+
+fun countries() = listOf(
+    Country("Japan", TimeZone.of("Asia/Tokyo")),
+    Country("France", TimeZone.of("Europe/Paris")),
+    Country("Mexico", TimeZone.of("America/Mexico_City")),
+    Country("Indonesia", TimeZone.of("Asia/Jakarta")),
+    Country("Egypt", TimeZone.of("Africa/Cairo")),
+)
+
 @Composable
-fun App() {
+fun App(countries: List<Country> = countries()) {
     MaterialTheme {
-        var location by remember { mutableStateOf("Europe/Paris") }
+        var showCountries by remember { mutableStateOf(false) }
         var timeAtLocation by remember { mutableStateOf("No location selected") }
 
         Column(modifier = Modifier.padding(20.dp)) {
@@ -36,36 +56,30 @@ fun App() {
                 modifier = Modifier.fillMaxWidth()
                     .align(Alignment.CenterHorizontally)
             )
-
-            TextField(
-                value = location,
-                modifier = Modifier.padding(top = 10.dp),
-                onValueChange = {
-                    location = it
+            Row(modifier = Modifier.padding(start = 20.dp, top = 10.dp)) {
+                DropdownMenu(
+                    expanded = showCountries,
+                    onDismissRequest = { showCountries = false }
+                ) {
+                    countries.forEach { (name, zone) ->
+                        DropdownMenuItem(
+                            onClick = {
+                                timeAtLocation = currentTimeAt(name, zone)
+                                showCountries = false
+                            }
+                        ) {
+                            Text(name)
+                        }
+                    }
                 }
-            )
+            }
 
             Button(
-                modifier = Modifier.padding(top = 10.dp),
-                onClick = {
-                    timeAtLocation = currentTimeAt(location) ?: "Invalid Location"
-                }
+                modifier = Modifier.padding(start = 20.dp, top = 10.dp),
+                onClick = { showCountries = !showCountries }
             ) {
-                Text("Show Time")
+                Text("Select Location")
             }
         }
-    }
-}
-
-fun currentTimeAt(location: String): String? {
-    fun LocalTime.formatted() = "$hour:$minute:$second"
-
-    return try {
-        val time = Clock.System.now()
-        val zone = TimeZone.of(location)
-        val localTime = time.toLocalDateTime(zone).time
-        "The time in $location is ${localTime.formatted()}"
-    } catch (ex: IllegalTimeZoneException) {
-        null
     }
 }
